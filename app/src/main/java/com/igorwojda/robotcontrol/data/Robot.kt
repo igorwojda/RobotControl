b8f550c9b7d218c7dca2c91762f1390e471e2bd4
Igor Wojda <igor.wojda@gmail.com>
2022-07-30 13:32:36 +0200

    Added coordinate delegate

--- app/src/main/java/com/igorwojda/robotcontrol/data/Robot.kt
+++ app/src/main/java/com/igorwojda/robotcontrol/data/Robot.kt
@@ -6,21 +6,8 @@ data class Robot(
     var coordinate: Coordinate,
     var orientation: Orientation,
 ) {
-    var coordinateX : Int
-        get() {
-            return coordinate.x
-        }
-        set(value) {
-            coordinate = coordinate.copy(x = value)
-        }
-
-    var coordinateY : Int
-        get() {
-            return coordinate.y
-        }
-        set(value) {
-            coordinate = coordinate.copy(y = value)
-        }
+    var coordinateX : Int by coordinate::x
+    var coordinateY : Int by coordinate::y
 
     val status: String
         get() = "${coordinate.x} ${coordinate.y} $orientation"
